63f98693343900e75f522214223eea538dfa5468
ShawnMalal <shawnmalal@gmail.com>
2023-01-30 15:25:17 -0800

    stringserver finished

--- NumberServer.java
+++ NumberServer.java
@@ -7,20 +7,11 @@ class Handler implements URLHandler {
     int num = 0;
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Shawn's " + "Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
+        if (url.getPath().equals("/add-message?s=")) {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-            }
+            return String.format("Shawn's " + "Number: %d", num);
+        } 
+         else {
             return "404 Not Found!";
         }
     }
--- StringServer.java
+++ StringServer.java
@@ -1,26 +1,32 @@
 import java.io.IOException;
 import java.net.URI;
 
-class Handler implements URLHandler {    
 
-    @Override
+class Handler implements URLHandler {
+    
+    String response = "";
+
     public String handleRequest(URI url) {
+
         if (url.getPath().equals("/")) {
-            return String.format("Shawn, " + "CSE, " + "15L");
+            return response;
+        } 
+        else if (url.getPath().contains("add")) {
+
+            String[] parameters;
+            parameters = url.getQuery().split("=");
+
+            if (parameters[0].equals("s")) {
+                response = response.concat(parameters[1]);
+                response = response.concat("\n");
+                return response;
+            }
+            return "Wrong url input!";
     }
-    else if (url.getPath().equals("/increment")) {
-        return String.format("Number incremented!");
-    } else {
-        System.out.println("Path: " + url.getPath());
-        if (url.getPath().contains("/add")) {
-            String[] parameters = url.getQuery().split("=");
-        }
-        return "404 Not Found!";
-    }
+    return "404 not found!";
 }
 
 class StringServer {
-        
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
@@ -31,12 +37,5 @@ class StringServer {
 
         Server.start(port, new Handler());
     }
-
 }
 }
-
-
-
-
-
-
